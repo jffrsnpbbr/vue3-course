b5a0fd68c37ad9bb5c654edaa6a3d6cdb2cc7fff
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-03 17:59:11 +0800

    [S0107] Re-building the App with Vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,12 +1,26 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-}
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+// }
 
-buttonEl.addEventListener('click', addGoal);
+// buttonEl.addEventListener('click', addGoal);
+
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: '',
+    };
+  },
+  methods: {
+    addGoals() {
+      this.goals.push(this.enteredValue);
+    }
+  }
+}).mount('#app');
