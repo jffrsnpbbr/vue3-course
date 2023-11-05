0d4cc8b3e8255a6abd2776e8ef149a3e78a226c8
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-06 03:40:56 +0800

    [S02L025] Exploring Event Modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,14 +2,25 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: '',
     };
   },
   methods: {
+    setName(event, lastname) {
+      this.name = event.target.value + ' ' + lastname;
+    },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
+    },
+    submitForm(event) {
+      alert('Submitted!');
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
     },
   },
 });
