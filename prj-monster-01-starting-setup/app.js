0b732c589ea7400a3b2acc70ea089ba11f649eaf
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-07 16:04:37 +0800

    [S04L056] Finishing the Core Functionality

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -8,14 +8,20 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
-      winner: null
+      winner: null,
     };
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.monsterHealth + '%' };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.playerHealth + '%' };
     },
     mayUseSpecialAttack() {
@@ -32,13 +38,19 @@ const app = Vue.createApp({
     },
     monsterHealth(value) {
       if (value <= 0 && this.playerHealh <= 0) {
-        this.winner = 'draw'
+        this.winner = 'draw';
       } else if (value <= 0) {
         this.winner = 'player';
       }
-    }
+    },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
@@ -65,5 +77,8 @@ const app = Vue.createApp({
       }
       this.attackPlayer();
     },
+    surrender() {
+      this.winner = 'monster';
+    }
   },
 }).mount('#game');
