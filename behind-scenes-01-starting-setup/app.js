38148f0667238af005d53fc10964f5f367fcce5e
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-09 07:09:44 +0800

    [S04L062] One App vs Multiple Apps

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -16,3 +16,37 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: 'Pizza'
+    }
+  }
+});
+
+app2.mount('#app2');
+
+// const data = {
+//   message: 'Hello',
+//   longMessage: 'Hello! World!',
+// };
+
+// const handler = {
+//   set(target, key, value) {
+//     if (key === 'message') {
+//       target.longMessage = value + ' World!';
+//     }
+//     target.message = value;
+//   },
+// };
+
+// const proxy = new Proxy(data, handler);
+
+// proxy.message = 'Hello!!!!';
+
+// console.log(proxy.longMessage);
+
+
+
+
