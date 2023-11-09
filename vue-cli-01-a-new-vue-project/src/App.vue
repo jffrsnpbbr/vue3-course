1053a7bed03a659a96d4b5f9719692b0cb270242
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-09 10:50:53 +0800

    [S07L085] Creating a Basic Vue App

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: '7b42c21a-b5ea-4519-9f9b-98a957f67ed7',
+          name: 'Manuel Lorenz',
+          phone: '0123 45678 90',
+          email: 'manuel.lorenz@example.com',
+        },
+        {
+          id: 'c6048872-4244-43e0-840b-d6426f5a1024',
+          name: 'Julie Jones',
+          phone: '0987 6543 21',
+          email: 'julie.jones@example.com',
+        },
+      ],
+    };
+  },
+};
+</script>
