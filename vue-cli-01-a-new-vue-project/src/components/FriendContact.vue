d86141f902f9e68783974652840cad4e8aaf22a1
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-09 11:07:41 +0800

    [S07L086] Adding a Component

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: '7b42c21a-b5ea-4519-9f9b-98a957f67ed7',
+        name: 'Manuel Lorenz',
+        phone: '0123 45678 90',
+        email: 'manuel.lorenz@example.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
