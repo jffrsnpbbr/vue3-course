21fe340df89ff6e358ed5dff5728084432871f24
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-12 17:20:09 +0800

    [S09L117] Dynamic Components

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,34 +1,32 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <!-- <course-goals>
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps.anotherProp }}</p>
-      </template>
-    </course-goals> -->
 
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps.anotherProp }}</p>
-    </course-goals>
+
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponents==='active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponents==='manage-goals'"></manage-goals> -->
+    <component :is="selectedComponents"></component>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
-
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 export default {
-  components: { TheHeader, BadgeList, UserInfo, CourseGoals },
+  components: {
+    TheHeader,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
+  },
   data() {
     return {
       activeUser: {
@@ -36,8 +34,14 @@ export default {
         description: 'Site owner and admin',
         role: 'admin',
       },
+      selectedComponents: 'active-goals'
     };
   },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponents = cmp;
+    }
+  }
 };
 </script>
 
