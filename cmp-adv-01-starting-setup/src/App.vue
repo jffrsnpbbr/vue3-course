580e9a7825ac87f9dddc18dc0ffc5e723dea37ae
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-12 17:01:36 +0800

    [S09L116] Scoped Lots

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -7,6 +7,17 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <!-- <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+      </template>
+    </course-goals> -->
+
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps.anotherProp }}</p>
+    </course-goals>
   </div>
 </template>
 
@@ -14,9 +25,10 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
-  components: { TheHeader, BadgeList, UserInfo },
+  components: { TheHeader, BadgeList, UserInfo, CourseGoals },
   data() {
     return {
       activeUser: {
