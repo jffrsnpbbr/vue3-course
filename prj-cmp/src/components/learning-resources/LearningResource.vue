7fae65582914a3a7f52bbc63e2b5f1614f2e427c
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-24 23:58:22 +0800

    [S10L128]First Components and Props

---
+++ prj-cmp/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">{{ title }}</a>
+    </nav>
+  </li>
+</template>
+<script>
+export default {
+  props: ['title', 'description', 'link']
+}
+</script>
+
+<style scoped></style>
