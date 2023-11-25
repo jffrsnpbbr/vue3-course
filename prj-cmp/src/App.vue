054df2ec233241fd12a2fb73ccadf3dce7879d55
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-25 09:03:41 +0800

    [S10L132] Dynamic Components & Attribute Fallthrough

--- prj-cmp/src/App.vue
+++ prj-cmp/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <the-header title="RememberMe"></the-header>
-  <StoredResources :resources="storedResources"></StoredResources>
+  <the-resources></the-resources>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
+
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
-    StoredResources,
-    TheHeader
+    TheHeader,
+    TheResources
   },
   data() {
     return {
---
+++ prj-cmp/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+  </base-card>
+  <base-card>
+    <component :is="selectedTab"></component>
+  </base-card>
+</template>
+
+<script>
+import AddResource from './AddResource.vue';
+import StoredResources from './StoredResources.vue';
+
+export default {
+  components: { StoredResources, AddResource },
+  data() {
+    return {
+      selectedTab: 'stored-resources'
+    }
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    }
+  }
+}
+</script>
+
+<style scoped></style>
