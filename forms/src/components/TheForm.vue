0dbaa8a914242da9342f2fc2b7d0b00572f88598
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-26 14:37:33 +0800

    [S11L143] V-model and dropdowns

--- forms/src/components/TheForm.vue
+++ forms/src/components/TheForm.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -55,7 +55,11 @@
 <script>
 export default {
   data() {
-    return { userName: '', userAge: null }
+    return {
+      userName: '',
+      userAge: null,
+      referrer: 'wom'
+    }
   },
   methods: {
     submitForm() {
@@ -64,6 +68,8 @@ export default {
       console.log(this.$refs.ageInput.value + 5);
       console.log(31);
       this.userAge = null;
+      console.log('referrer ' + this.referrer);
+      this.referrer = 'wom';
     }
   }
 }
