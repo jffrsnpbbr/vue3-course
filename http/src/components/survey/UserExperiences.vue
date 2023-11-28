fa2547619941a1bc002b8503431b77891376388b
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-29 02:00:02 +0800

    [S12157]  Getting Data (Get Request) & Transforming Response Data

--- http/src/components/survey/UserExperiences.vue
+++ http/src/components/survey/UserExperiences.vue
@@ -3,27 +3,46 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
-        <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
-        ></survey-result>
+        <survey-result v-for="result in results" :key="result.id" :name="result.name"
+          :rating="result.rating"></survey-result>
       </ul>
     </base-card>
   </section>
 </template>
 
 <script>
+import axios from 'axios';
 import SurveyResult from './SurveyResult.vue';
 
 export default {
   // props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: []
+    }
+  },
+  methods: {
+    loadExperiences() {
+      fetch('https://vue-http-demo-766ad-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json')
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        }).then((data) => {
+          const results = [];
+          for (const id in data) {
+            results.push({ id: id, name: data[id].name, rating: data[id].rating })
+          }
+          this.results = results;
+        })
+    }
+
   },
 };
 </script>
