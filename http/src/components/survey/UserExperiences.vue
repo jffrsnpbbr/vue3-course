a87e614ff8e9e4d2710b30175365eef7facb08e7
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-12-01 18:55:34 +0800

    [S12L158] Loading Data when components mounts

--- http/src/components/survey/UserExperiences.vue
+++ http/src/components/survey/UserExperiences.vue
@@ -6,21 +6,24 @@
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
-        <survey-result v-for="result in results" :key="result.id" :name="result.name"
-          :rating="result.rating"></survey-result>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
       </ul>
     </base-card>
   </section>
 </template>
 
 <script>
-import axios from 'axios';
-import SurveyResult from './SurveyResult.vue';
+import SurveyResult from './SurveyResult.vue'
 
 export default {
   // props: ['results'],
   components: {
-    SurveyResult,
+    SurveyResult
   },
   data() {
     return {
@@ -29,22 +32,27 @@ export default {
   },
   methods: {
     loadExperiences() {
-      fetch('https://vue-http-demo-766ad-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json')
+      fetch(
+        'https://vue-http-demo-766ad-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json'
+      )
         .then((response) => {
           if (response.ok) {
-            return response.json();
+            return response.json()
           }
-        }).then((data) => {
-          const results = [];
+        })
+        .then((data) => {
+          const results = []
           for (const id in data) {
             results.push({ id: id, name: data[id].name, rating: data[id].rating })
           }
-          this.results = results;
+          this.results = results
         })
     }
-
   },
-};
+  mounted() {
+    this.loadExperiences()
+  }
+}
 </script>
 
 <style scoped>
